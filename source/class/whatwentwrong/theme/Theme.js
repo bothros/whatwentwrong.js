e173c3fdb387016f7b5715d01260cfd8b3a5aed7
bothros <herpestesfantasticus@gmail.com>
2012-01-24 22:46:26 -0500

    New qooxdoo project

---
+++ source/class/whatwentwrong/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("whatwentwrong.theme.Theme",
+{
+  meta :
+  {
+    color : whatwentwrong.theme.Color,
+    decoration : whatwentwrong.theme.Decoration,
+    font : whatwentwrong.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : whatwentwrong.theme.Appearance
+  }
+});
